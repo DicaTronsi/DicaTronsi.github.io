3130b10e8995e01404774648eb8c1f15e2b2a93c
DicaTronsi <64757778+DicaTronsi@users.noreply.github.com>
2020-05-04 16:28:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const canvas = document.getElementById('back');
 const context = canvas.getContext('2d');
 
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+context.scale(20,20);
+
+function drawEnemy(x, y, type) {
+  context.fillStyle = 'white';
+  context.fillRect(x, y, 1, 1);
+  
+}
+ 
+drawEnemy(1, 1, 2)
